7a85a0d6a8fe8150f71b91c3a7606681a2a61e00
moreno27 <alejanmm2000@gmail.com>
2021-04-28 11:26:33 +0200

    He sobrecargado el metodo escribirFicheroJSON para que tambien acepte objetos de tipo App demas de una lista de Apps

--- src/main/java/tarea/ServicioFicheroJSON.java
+++ src/main/java/tarea/ServicioFicheroJSON.java
@@ -26,6 +26,19 @@ public class ServicioFicheroJSON {
         ServicioFicheroJSON.escribirFicheroJSON(listaAplicaciones, idArchivo);
     }
 
+    /** <html>
+     * Genera un fichero con la extension json a partir de una lista que
+     * recibimos como parametro.
+     * <pre>
+     *Se genera un archivo con la informacion de todos los objetos que tenemos
+     *en la lista
+     * </pre>
+     *
+     * @param listaAplicaciones Lista de objetos de tipo App
+     * @param idArchivo Directorio del archivo
+     * @throws IOException Excepcion de malformacion del archivo
+     * </html>
+     */
     public static void escribirFicheroJSON(ArrayList<App> listaAplicaciones, String idArchivo) throws IOException {
 
         ObjectMapper mapeador = new ObjectMapper();
@@ -34,7 +47,34 @@ public class ServicioFicheroJSON {
 
         // Escribe en un fichero JSON el catálogo de muebles
         mapeador.writeValue(new File(idArchivo), App.generarListaApp(10));
-        
+
         System.out.println("El fichero " + idArchivo + " se ha creado correctamente");
     }
+
+    /** <html>
+     * Genera un fichero con la extension json a partir de un objeto de tipo App
+     * que recibimos como parametro.
+     * <pre>
+     *Se genera un archivo con la informacion del objeto que tenemos en la lista
+     * </pre>
+     *
+     * @param aplicaion Objeto de tipo App
+     * @param idArchivo Directorio donde se creara el archivo
+     * @throws IOException Excepcion de malformacion del archivo
+     * </html>
+     */
+    public static void escribirFicheroJSON(App aplicaion, String idArchivo) throws IOException {
+
+        ObjectMapper mapeador = new ObjectMapper();
+
+        mapeador.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+        // Escribe en un fichero JSON el catálogo de muebles
+        mapeador.writeValue(new File(idArchivo), aplicaion);
+
+        System.out.println("El fichero " + idArchivo + " se ha creado correctamente");
+    }
+    
+    /*HEMOS HECHO UNA SOBRECARGA DEL METODO PARA QUE SE PUDIERA REALIZAR EL 
+    ULTIMO APARTADO DEL EJERICIO*/
 }
